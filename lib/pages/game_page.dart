a50c2c5f54be5437eff87bd083f54a6b8a78668b
gaoz <gaoz@YuancxdeMacBook-Pro.local>
2019-08-16 13:49:21 +0800

    fix

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -4,9 +4,15 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_moecony/common/redux/state.dart';
 
 import 'package:flutter_moecony/widget/tabbar.dart';
+import 'package:flutter_moecony/widget/game/home_tab.dart';
+import 'package:flutter_moecony/widget/game/daily_tab.dart';
+import 'package:flutter_moecony/widget/game/map_tab.dart';
+import 'package:flutter_moecony/widget/game/task_tab.dart';
+import 'package:flutter_moecony/widget/game/activity_tab.dart';
 
 class GamePage extends StatefulWidget {
   static final String sName = 'game';
+
   @override
   _GamePageState createState() => _GamePageState();
 }
@@ -15,11 +21,19 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
   List<String> tab;
   TabController _tabController;
   int _currentIndex;
+  List<Widget> _tabView;
 
   @override
   void initState() {
     super.initState();
     tab = ['主页', '日常', '地图', '任务', '活动'];
+    _tabView = [
+      HomeTab(),
+      DailyTab(),
+      MapTab(),
+      TaskTab(),
+      ActivityTab(),
+    ];
     _currentIndex = 0;
     _tabController = TabController(length: tab.length, vsync: this);
     _tabController.addListener(_handleTabSelection);
@@ -35,6 +49,7 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
     if (mounted) {
       setState(() {
         _currentIndex = _tabController.index;
+        print(_currentIndex);
       });
     }
   }
@@ -64,13 +79,9 @@ class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
     return Expanded(
       child: TabBarView(
         controller: _tabController,
-        children: tab
-            .map(
-              (item) => Center(
-                child: Text(item),
-              ),
-            )
-            .toList(),
+        children: _tabView.map((item) {
+          return item;
+        }).toList(),
       ),
     );
   }
---
+++ lib/widget/game/map_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('MapTab'),
+      ),
+    );
+  }
+}
